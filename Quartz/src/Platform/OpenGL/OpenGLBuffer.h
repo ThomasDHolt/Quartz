d6301dcdbc6484923320c8edbc438a56e3a7fd25
ThomasHoltHullCS <T.D.Holt-2021@hull.ac.uk>
2023-10-11 19:00:38 +0100

    - Implemented a BufferLayout class which allows us to define a layout for the vertices we pass to our graphics API, meaning we can now define vertex positions, vertex colours and more.

--- Quartz/src/Platform/OpenGL/OpenGLBuffer.h
+++ Quartz/src/Platform/OpenGL/OpenGLBuffer.h
@@ -10,10 +10,14 @@ namespace Quartz
 		OpenGLVertexBuffer(float* pVertices, uint32_t pSize);
 		virtual ~OpenGLVertexBuffer();
 
-		virtual void Bind() const;
-		virtual void Unbind() const;
+		virtual void Bind() const override;
+		virtual void Unbind() const override;
+
+		virtual const BufferLayout& GetLayout() const override { return m_Layout; }
+		virtual void SetLayout(const BufferLayout& pLayout) override { m_Layout = pLayout; }
 	private:
 		uint32_t m_RendererID;
+		BufferLayout m_Layout;
 	};
 
 	class OpenGLIndexBuffer : public IndexBuffer
@@ -22,8 +26,8 @@ namespace Quartz
 		OpenGLIndexBuffer(uint32_t* pIndices, uint32_t pCount);
 		virtual ~OpenGLIndexBuffer();
 
-		virtual void Bind() const;
-		virtual void Unbind() const;
+		virtual void Bind() const override;
+		virtual void Unbind() const override;
 
 		virtual uint32_t GetCount() const { return m_Count; }
 	private:
